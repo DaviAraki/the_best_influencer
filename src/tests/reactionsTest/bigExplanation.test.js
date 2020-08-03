ba982ab98e5a7b9dcf0d44487b80218f17b5dc33
DaviAraki <davi.araki@gmail.com>
2020-08-03 10:18:09 -0300

    Create bigExplanation.test.js

---
+++ src/tests/reactionsTest/bigExplanation.test.js
@@ -0,0 +1,32 @@
+import bigExplanation from "../../components/Cards/Reactions/bigExplanation";
+import { draw, shuffleDeck, playCard } from "../../Game";
+import iTreta from "../../Game";
+import generateUniqueId from '../../utils/generateUniqueId'
+import boldClaim from "../../components/Cards/Reactions/boldClaim";
+import goodWill from "../../components/Cards/Reactions/goodWill";
+
+
+it("When i draw, the top card of my deck goes to my hand", () => {
+    const f = new bigExplanation();
+    const c = new goodWill()
+    const G = {
+        players: [
+            {
+                id: generateUniqueId(),
+                hand: [f],
+                board: { red: [], yellow: [], green: [] },
+                likes: 0,
+                reports: 0
+            },
+        ],
+        offer: {
+            deck: [c],
+            discardPile: []
+        }
+    };
+    playCard(G, { currentPlayer: "0" }, 0, 0);
+    expect(G.players[0].likes).toEqual(1);
+    expect(G.players[0].board.green[0]).toEqual(c);
+    expect(G.offer.discardPile[0]).toEqual[f]
+
+});
