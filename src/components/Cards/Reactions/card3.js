344e5147f406ba8aa3b3ff85ecef1a5977a3f449
Davi Araki <davi.araki@gmail.com>
2021-04-07 12:31:19 -0300

    new cards created

---
+++ src/components/Cards/Reactions/card3.js
@@ -0,0 +1,23 @@
+import basicReaction from "./basicReaction";
+
+export default class webRage extends basicReaction {
+    constructor() {
+        super({
+            name: "Web Rage",
+            green: { reports: 0, likes: 1 },
+            yellow: { reports: 0, likes: 1 },
+            red: { reports: 0, likes: 3 },
+            use: 8,
+            textBox:"current player: +🃏 -👍" ,
+            sheet:"🃏👍🚫"
+        })
+    }
+
+    static create(qty) {
+        let cards = [];
+        for (let i = 0; i < qty; i++) {
+            cards.push(new webRage());
+        }
+        return cards;
+    }
+}
---
+++ src/components/Cards/Reactions/card9.js
@@ -0,0 +1,23 @@
+import basicReaction from "./basicReaction";
+
+export default class webRage extends basicReaction {
+    constructor() {
+        super({
+            name: "Web Rage",
+            green: { reports: 0, likes: 1 },
+            yellow: { reports: 0, likes: 1 },
+            red: { reports: 0, likes: 3 },
+            use: 8,
+            textBox: "Target Player: +🟥 +🚫 ",
+            sheet: "🃏👍🚫🟩🟨🟥"
+        })
+    }
+
+    static create(qty) {
+        let cards = [];
+        for (let i = 0; i < qty; i++) {
+            cards.push(new webRage());
+        }
+        return cards;
+    }
+}
